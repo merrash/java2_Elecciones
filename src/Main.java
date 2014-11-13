0055bae4de91c68413db7c2244608f3ac99192d7
ibalenciaga <balenciaga@hotmail.com>
2014-11-13 12:08:10 +0100

    terminado

--- src/Main.java
+++ src/Main.java
@@ -53,7 +53,7 @@ public class Main
 
 					//pide el nombre del habitante y lo asigna a nombre de Habitante
 					System.out.print("\nNombre del habitante: ");
-					cadena = sc.nextLine();
+					cadena = sc.next();
 					habitante.setNombre(cadena);
 
 					//pide el apellido del habitante y lo asigna a apellido de Habitante
@@ -87,12 +87,13 @@ public class Main
 					habitante.setPoblacion(cadena);
 
 					//muestra el contenido de los atributos de Habitante
-					System.out.print("\nEl nombre del habitante es: " + habitante.getNombre()+ "\n");
-					System.out.print("\nEl apellido del habitante es: " + habitante.getApellido()+ "\n");
-					System.out.print("\nEl DNI del habitante es: " + habitante.getDni()+ "\n");
-					System.out.print("\nLa edad del habitante es: " + habitante.getEdad()+ "\n");
-					System.out.print("\nEl sexo del habitante es: " + habitante.getSexo()+ "\n");
-					System.out.print("\nEl telefono del habitante es: " + habitante.getTelefono()+ "\n");
+					System.out.print("\n****INFORMACION DEL HABITANTE****\n");
+					System.out.print("\nEl nombre del habitante es: " + habitante.getNombre()+ "");
+					System.out.print("\nEl apellido del habitante es: " + habitante.getApellido()+ "");
+					System.out.print("\nEl DNI del habitante es: " + habitante.getDni()+ "");
+					System.out.print("\nLa edad del habitante es: " + habitante.getEdad()+ "");
+					System.out.print("\nEl sexo del habitante es: " + habitante.getSexo()+ "");
+					System.out.print("\nEl telefono del habitante es: " + habitante.getTelefono()+ "");
 					System.out.print("\nLa poblacion del habitante es: " + habitante.getPoblacion()+ "\n");
 					
 					break;
@@ -129,9 +130,10 @@ public class Main
 					inmueble.setMetros(num);
 
 					//muestra el contenido de los atributos de Inmueble
-					System.out.print("\nEl ID del inmueble es: " + inmueble.getId()+ "\n");
-					System.out.print("\nEl tipo de inmueble es: " + inmueble.getTipo()+ "\n");
-					System.out.print("\nLa dirrecion del inmueble es: " + inmueble.getDireccion()+ "\n");
+					System.out.print("\n****INFORMACION DEL INMUEBLE****\n");
+					System.out.print("\nEl ID del inmueble es: " + inmueble.getId()+ "");
+					System.out.print("\nEl tipo de inmueble es: " + inmueble.getTipo()+ "");
+					System.out.print("\nLa dirrecion del inmueble es: " + inmueble.getDireccion()+ "");
 					System.out.print("\nLos metros del inmueble son: " + inmueble.getMetros()+ "\n");
 					
 					break;
@@ -177,11 +179,12 @@ public class Main
 					espaciopublico.setMetros(num);
 
 					//muestra el contenido de los atributos de EspacioPublico
-					System.out.print("\nEl ID del espacio publico es: " + espaciopublico.getId()+ "\n");
-					System.out.print("\nEl ID del espacio publico es: " + espaciopublico.getNombre()+ "\n");
-					System.out.print("\nEl tipo del espacio publico es: " + espaciopublico.getTipo()+ "\n");
-					System.out.print("\nLa direccion del espacio publico es: " + espaciopublico.getDireccion()+ "\n");
-					System.out.print("\nLa poblacion del espacio publico es: " + espaciopublico.getPoblacion()+ "\n");
+					System.out.print("\n****INFORMACION DEL ESPACIO PUBLICO****\n");
+					System.out.print("\nEl ID del espacio publico es: " + espaciopublico.getId()+ "");
+					System.out.print("\nEl nombre del espacio publico es: " + espaciopublico.getNombre()+ "");
+					System.out.print("\nEl tipo del espacio publico es: " + espaciopublico.getTipo()+ "");
+					System.out.print("\nLa direccion del espacio publico es: " + espaciopublico.getDireccion()+ "");
+					System.out.print("\nLa poblacion del espacio publico es: " + espaciopublico.getPoblacion()+ "");
 					System.out.print("\nLos metros del espacio publico es: " + espaciopublico.getMetros()+ "\n");
 					
 					break;
@@ -189,96 +192,100 @@ public class Main
 				}
 
 
-				case "ayuntamiento":
+				case "aayuntamiento":
 				{
 
-					System.out.print("AYUNTAMIENTO OK");
-					break;
-
-					/*
+					
 					//**************************** AYUNTAMIENTO ****************************************
 
 					Ayuntamiento ayuntamiento = new Ayuntamiento(0,"","","");
 
-
-					System.out.print("\nID del ayuntamiento: ");//pide el ID del ayuntamiento
+					//pide el ID del ayuntamiento y lo asigna a id de Ayuntamiento
+					System.out.print("\nID del ayuntamiento: ");
 					num = sc.nextInt();
 					ayuntamiento.setId(num);
 
-					System.out.print("\nPoblacion del ayuntamiento: ");//pide la poblacion del ayuntamiento
+					//pide la poblacion del ayuntamiento y lo asigna a poblacion de Ayuntamiento
+					System.out.print("\nPoblacion del ayuntamiento: ");
 					cadena = sc.next();
 					ayuntamiento.setPoblacion(cadena);
 					
-					System.out.print("\nLa direccion del ayuntamiento: ");//pide la direccion del ayuntamiento
+					//pide la direccion del ayuntamiento y lo asigna a direccion de Ayuntamiento
+					System.out.print("\nLa direccion del ayuntamiento: ");
 					cadena = sc.next();
 					ayuntamiento.setDireccion(cadena);
 
-
-					System.out.print("\nEl nombre del alcalde: ");//pide el nombre del alcalde
+					//pide el nombre del alcalde y lo asigna a nombrealcalde de Ayuntamiento
+					System.out.print("\nEl nombre del alcalde: ");
 					cadena = sc.next();
 					ayuntamiento.setNombreAlcalde(cadena);
 
+					//muestra el contenido de los atributos de Ayuntamiento
+					System.out.print("\n****INFORMACION DEL AYUNTAMIENTO****\n");
+					System.out.print("\nEl ID del ayuntamiento es: " + ayuntamiento.getId()+ "");
+					System.out.print("\nLa poblacion del ayuntamiento es: " + ayuntamiento.getPoblacion()+ "");
+					System.out.print("\nLa direcciondel ayuntamiento es: " + ayuntamiento.getDireccion()+ "");
+					System.out.print("\nEl nombre del alcalde es: " + ayuntamiento.getNombreAlcalde()+ "\n");
 					
-					System.out.print("\nEl ID del ayuntamiento es: " + ayuntamiento.getId()+ "\n");//muestra el ID del ayuntamiento
-					System.out.print("\nLa poblacion del ayuntamiento es: " + ayuntamiento.getPoblacion()+ "\n");//muestra la poblacion del ayuntamiento
-					System.out.print("\nLa direcciondel ayuntamiento es: " + ayuntamiento.getDireccion()+ "\n");//muestra la dirrecion del ayuntamiento
-					System.out.print("\nEl nombre del alcalde es: " + ayuntamiento.getNombreAlcalde()+ "\n");//muestra el nombre del alcalde
-					*/
+					break;
+
 				}
 
 				case "apartido":
 				{
 
-					System.out.print("APARTIDO OK");
-					break;
-					/*
-					//**************PARTIDO POLITICO*****************************************
+					
+					//*************************************PARTIDO POLITICO*****************************************
 
 					Partido partido = new Partido(0,"","",0,"");
 
-
-					System.out.print("\nId del partido");//pide el ID del partido
+					//pide el ID del partido y lo asigna a id de Partido
+					System.out.print("\nId del partido");
 					num = sc.nextInt();
 					partido.setId(num);
 
-					System.out.print("\nNombre del partido: ");//pide el nombre del partido
+					//pide el nombre del partido y lo asigna a nombre de Partido
+					System.out.print("\nNombre del partido: ");
 					cadena = sc.next();
 					partido.setNombre(cadena);
 
-					System.out.print("\nSiglas del partido: ");//pide las siglas del partido
+					//pide las siglas del partido y lo asigna a siglas de Partido
+					System.out.print("\nSiglas del partido: ");
 					cadena = sc.next();
 					partido.setSiglas(cadena);
 
-					System.out.print("\nNumero de afiliados en el partido: ");//pide el numero de afiliados en el partido
+					//pide el numero de afiliados del partido y lo asigna a afiliados de Partido
+					System.out.print("\nNumero de afiliados en el partido: ");
 					num = sc.nextInt();
 					partido.setAfiliados(num);
-
-					System.out.print("\nNombre del secretario general: ");//pide el nombre del secretario general del partido
+					//pide el nombre del secretario general del partido y lo asigna a secretario de Partido
+					System.out.print("\nNombre del secretario general: ");
 					cadena = sc.next();
 					partido.setSecretario(cadena);
 
+					//muestra el contenido de los atributos de Partido
+					System.out.print("\n****INFORMACION DEL PARTIDO****\n");	
+					System.out.print("\nEl id del partido es: " + partido.getId()+ "");
+					System.out.print("\nEl nombre del partido es: " + partido.getNombre()+ "");
+					System.out.print("\nLas siglas del partido son" + partido.getSiglas()+ "");
+					System.out.print("\nEl numero de afiliados es: " + partido.getAfiliados()+ "");
+					System.out.print("\nEl nombre del secretario general es: " + partido.getSecretario()+ "\n");
 							
-					System.out.print("\nEl id del partido es: " + partido.getId()+ "\n");//muestra el ID del partido
-					System.out.print("\nEl nombre del partido es: " + partido.getNombre()+ "\n");//muestra el nombre del partido
-					System.out.print("\nLas siglas del partido son" + partido.getSiglas()+ "\n");//muestra las siglas del partido
-					System.out.print("\nEl numero de afiliados es: " + partido.getAfiliados()+ "\n");//muestra el numero de afiliados
-					System.out.print("\nEl nombre del secretario general es: " + partido.getSecretario()+ "\n");//muestra el nombre del secretario
-							
-					*/
+					break;
+
 				}
 
 				case "lpartidos":
 				{
 
-					System.out.print("LEER PARTIDO OK");
-					break;
-					/*
-					//************LEER ARCHIVO PARTIDO Y GUARDARLO EN EL ARRAYLIST*******************************************
+															
+					//*****************LEER ARCHIVO PARTIDO Y GUARDARLO EN EL ARRAYLIST*************************************
+					
 					try
 					{
 					
 			        	//File archivo = new File ("/home/zubiri/ProyectosJava/java2_Elecciones/src/listadoPartidos.txt");
-			        	File archivo = new File ("/home/zubiri/ProyectosJava/java2_Elecciones/src/listadoPartidos.txt");
+			        	File archivo = new File ("/home/ibalenciaga/ProyectosJava/java2_Elecciones/src/listadoPartidos.txt");
 			        	
 			        	//FileReader lector = new FileReader (archivo);
 			        	//BufferedReader bufer = new BufferedReader(lector);
@@ -289,7 +296,9 @@ public class Main
 			        	//Cremos el objeto Arraylist del tipo partido
 			        	ArrayList <Partido> partido = new ArrayList <Partido>() ; 
 			        	
-			        	
+			        	if (linea.hasNextLine())
+			        		System.out.print("\n****INFORMACION DE LOS PARTIDOS****\n");
+						
 						while (linea.hasNextLine())
 						{     
 
@@ -300,48 +309,49 @@ public class Main
 							String[] separadas = concoma.split(",");
 
 							//creamos el nuevo partido y le asignamos los datos obtenido en el String separadas.
-							Partido aux = new Partido(Integer.parseInt(separadas[0]),separadas[1],separadas[2],Integer.parseInt(separadas[3]),separadas[4]);
-						
+							Partido aux = new Partido(Integer.parseInt(separadas[0]),separadas[1],separadas[2],Integer.parseInt(separadas[3]),
+																															separadas[4]);
+							
+							/*
 							//estas sentencias es lo mismo que poner la linea de arriba
 							
 							//ponemos los datos en los atributos del objeto
-							//aux.setId(Integer.parseInt(separadas[0]));
-							//aux.setNombre(separadas[1]);
-							///aux.setSiglas(separadas[2]);
-							//aux.setAfiliados(Integer.parseInt(separadas[3]));
-							//aux.setSecretario(separadas[4]);
-							
+							aux.setId(Integer.parseInt(separadas[0]));
+							aux.setNombre(separadas[1]);
+							aux.setSiglas(separadas[2]);
+							aux.setAfiliados(Integer.parseInt(separadas[3]));
+							aux.setSecretario(separadas[4]);
+							*/
 
 							//añadimos el objeto aux al ArrayList
 							partido.add(aux);
 
 							//obtenemos los datos del ultimo partido añadido
-							System.out.println("\nEste es el ID del partido: " + aux.getId());
-							System.out.println("\nNombre del partido: " + aux.getNombre());
-							System.out.println("\nSiglas del partido: " + aux.getSiglas());
-							System.out.println("\nAfiliados del partido: " + aux.getAfiliados());
-							System.out.println("\nSecretario general: " + aux.getSecretario() + "\n");
+
+							System.out.println("Este es el ID del partido: " + aux.getId());
+							System.out.println("Nombre del partido: " + aux.getNombre());
+							System.out.println("Siglas del partido: " + aux.getSiglas());
+							System.out.println("Afiliados del partido: " + aux.getAfiliados());
+							System.out.println("Secretario general: " + aux.getSecretario() + "\n");
 						
 						}
-									
-						System.out.println("antes del vaciado del arraylist PARTIDO:" + (partido.size()-1) + "elementos") ; 		
-						System.out.println("antes del vaciado del arraylist AUX:" + (aux.size()-1) + "elementos") ; 		
-
+								
+						System.out.print("\n****FIN DE LOS PARTIDOS****\n");
+						System.out.println("antes del vaciado del arraylist PARTIDO:" + partido.size() + "elementos") ; 		
+						
 						//vaciamos los ArryList
 						partido.clear();
-						aux.clear();
-
-						System.out.println("despues del vaciado del arraylist PARTIDO:" + partido.size() + "elementos") ; 
-						System.out.println("despues del vaciado del arraylist AUX:" + aux.size() + "elementos") ; 
 						
-
+						System.out.println("despues del vaciado del arraylist PARTIDO:" + partido.size() + "elementos") ; 
+						
 			        }
 			        
 			        catch(Exception e)
 			        {
 			        	e.printStackTrace();
 			        }
-			        */
+			        
+			        break;
 
 		    	}
 
@@ -349,17 +359,14 @@ public class Main
 				case "lcenso":
 				{
 
-					System.out.print("LEER CENSO OK");
-					break;
-
-			        /*
-			        //************LEER FICHERO CENSO Y GUARDAR EN ARRAYLIST. IMPRIMIR SOLO A LOS MAYORES DE 18 AÑOS
+					
+			        //************LEER FICHERO CENSO Y GUARDAR EN ARRAYLIST. IMPRIMIR SOLO A LOS MAYORES DE 18 AÑOS*************
 
 			        try
 					{
 					
 			        	//File archivo = new File ("/home/zubiri/ProyectosJava/java2_Elecciones/src/censo.txt");
-			        	File archivo = new File ("/home/zubiri/ProyectosJava/java2_Elecciones/src/censo.txt");
+			        	File archivo = new File ("/home/ibalenciaga/ProyectosJava/java2_Elecciones/src/censo.txt");
 			        	
 			        	//FileReader lector = new FileReader (archivo);
 			        	//BufferedReader bufer = new BufferedReader(lector);
@@ -367,9 +374,11 @@ public class Main
 			        	
 			        	//ArrayList <String> lista = new ArrayList <String>() ;
 
-			        	//Cremos el objeto Arraylist del tipo habitante
+			        	//Creamos el objeto Arraylist del tipo habitante
 			        	ArrayList <Habitante> habitante = new ArrayList <Habitante>() ; 
 			        	
+			        	if (linea.hasNextLine())
+			        		System.out.print("\n****INFORMACION DEL CENSO****\n");
 			        	
 						while (linea.hasNextLine())
 						{     
@@ -377,23 +386,25 @@ public class Main
 							//obtenemos la linea
 			 				String concoma = linea.nextLine();
 
-			 				//asigno al array los atributos de la fila leida sin comas
+			 				//asigna al array los atributos de la fila leida sin comas
 							String[] separadas = concoma.split(",");
 
 							//creamos el nuevo partido y le asignamos los datos obtenido en el String separadas.
-							Habitante aux = new Habitante(separadas[0],separadas[1],separadas[2],Integer.parseInt(separadas[3]),separadas[4],Integer.parseInt(separadas[5]),separadas[6]);
+							Habitante aux = new Habitante(separadas[0],separadas[1],separadas[2],Integer.parseInt(separadas[3]),separadas[4],
+																								Integer.parseInt(separadas[5]),separadas[6]);
 						
+							/*
 							//estas sentencias es lo mismo que poner la linea de arriba
 							
 							//ponemos los datos en los atributos del objeto
-							//aux.setNombre(separadas[0]);
-							//aux.setApellido(separadas[1]);
-							//aux.setDni(separadas[2]);
-							//aux.setEdad(Integer.parseInt(separadas[3]));
-							//aux.setSexo(separadas[4]);
-							//aux.setTelefono(Integer.parseInt(separadas[5]));
-							//aux.setPoblacion(separadas[6]);
-							
+							aux.setNombre(separadas[0]);
+							aux.setApellido(separadas[1]);
+							aux.setDni(separadas[2]);
+							aux.setEdad(Integer.parseInt(separadas[3]));
+							aux.setSexo(separadas[4]);
+							aux.setTelefono(Integer.parseInt(separadas[5]));
+							aux.setPoblacion(separadas[6]);
+							*/
 
 
 							//Comprobamos si es mayor de edad
@@ -403,27 +414,27 @@ public class Main
 								//añadimos el objeto aux al ArrayList
 								habitante.add(aux);
 
-								//obtenemos los datos del ultimo partido añadido
-								System.out.println("\nNombre del habitante: " + aux.getNombre());
-								System.out.println("\nApellido del habitante: " + aux.getApellido());
-								System.out.println("\nDNI del habitante: " + aux.getDni());
-								System.out.println("\nEdad del habitante: " + aux.getEdad());
-								System.out.println("\nSexo del habitante " + aux.getSexo());
-								System.out.println("\nTelefono del habitante: " + aux.getTelefono());
-								System.out.println("\nPoblacion del habitante: " + aux.getPoblacion() + "\n");
+								//obtenemos los datos del ultimo habitante añadido
+								System.out.println("Nombre del habitante: " + aux.getNombre());
+								System.out.println("Apellido del habitante: " + aux.getApellido());
+								System.out.println("DNI del habitante: " + aux.getDni());
+								System.out.println("Edad del habitante: " + aux.getEdad());
+								System.out.println("Sexo del habitante " + aux.getSexo());
+								System.out.println("Telefono del habitante: " + aux.getTelefono());
+								System.out.println("Poblacion del habitante: " + aux.getPoblacion() + "\n");
 						
 							}
 
 						}
-								System.out.println("antes del vaciado del arraylist HABITANTE:" + (habitante.size()-1) + "elementos") ; 		
-								System.out.println("antes del vaciado del arraylist AUX:" + (aux.size()-1) + "elementos") ; 		
+								System.out.println("antes del vaciado del arraylist HABITANTE:" + habitante.size() + "elementos") ; 		
+								
 
 								//vaciamos los ArryList
 								habitante.clear();
-								aux.clear();
+								
 
 								System.out.println("despues del vaciado del arraylist HABITANTE:" + habitante.size() + "elementos") ; 
-								System.out.println("despues del vaciado del arraylist AUX:" + aux.size() + "elementos") ; 
+								
 								
 
 			        }
@@ -432,11 +443,14 @@ public class Main
 			        {
 			        	e.printStackTrace();
 			        }
-			        */
+			        
+			        break;
+
 		    	}
 
 		    	case "salir":
-		    	break;
+		    		
+		    		break;
 
 		    	default:
 		    		System.out.println("No has elegido la opcion correcta");
